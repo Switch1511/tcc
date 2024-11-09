3b5813bd95fb21a671995dc4a680f632030b4613
Switch1511 <gustavopasqualli0@gmail.com>
2024-11-09 17:58:40 -0300

    ajustes

--- back/index.js
+++ back/index.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const app = express();
 const port = process.env.port || 3000;
 const path = require('path');
-
+ 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
